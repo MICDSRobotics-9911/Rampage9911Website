8da9303d6456018c4a8e6b3062054becd5f3b3ba
stlgolfer <stlgolfer@live.com>
2017-07-16 14:23:56 -0500

    Some style sheets done, router is ok

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,27 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
+import { HomeComponent } from './components/home/home.component';
+
+const appRoutes: Routes = [
+        {
+                path: 'home',
+                component: HomeComponent
+        }
+]
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+        declarations: [
+                AppComponent,
+                HomeComponent
+        ],
+        imports: [
+                BrowserModule,
+                RouterModule.forRoot(appRoutes)
+        ],
+        providers: [],
+        bootstrap: [AppComponent]
 })
 export class AppModule { }
