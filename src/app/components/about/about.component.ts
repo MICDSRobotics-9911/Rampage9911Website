36c5a24ffb909f17bc521cd6eefca0db76716e65
stlgolfer <stlgolfer@live.com>
2017-07-18 14:43:02 -0500

    About page auto-gen passing (needs formattting)

--- src/app/components/about/about.component.ts
+++ src/app/components/about/about.component.ts
@@ -9,14 +9,39 @@ export class AboutComponent implements OnInit {
         imgConfig = {
                 "people": [
                         {
-                                "name":"Alex M",
-                                "bio":"Sup G?",
-                                "imgpath":"../../../assets/team/alex.jpg"
+                                "name":"Kendall",
+                                "bio":"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur pellentesque sagittis est nec vehicula. Integer porttitor vestibulum orci, vel fermentum ligula luctus non. Proin a suscipit nisl. Pellentesque malesuada nec ipsum vel consectetur. Donec a gravida turpis. Fusce sagittis molestie mauris, ut porta turpis imperdiet sit amet. Curabitur arcu ligula, scelerisque sit amet facilisis id, vehicula sed turpis. Sed faucibus odio a convallis porttitor. Curabitur ut odio consectetur ligula vulputate facilisis. Praesent volutpat pellentesque purus at lobortis. Suspendisse quis enim a libero condimentum consequat. Donec consequat magna nec tellus venenatis sollicitudin. Proin dui justo, mollis sed sodales eget, laoreet sed nisl. Curabitur vehicula est ac eros condimentum pellentesque.",
+                                "imgpath":"../../../assets/team/kendall.jpg"
+                        },
+                        {
+                                "name":"Megan",
+                                "bio":"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur pellentesque sagittis est nec vehicula. Integer porttitor vestibulum orci, vel fermentum ligula luctus non. Proin a suscipit nisl. Pellentesque malesuada nec ipsum vel consectetur. Donec a gravida turpis. Fusce sagittis molestie mauris, ut porta turpis imperdiet sit amet. Curabitur arcu ligula, scelerisque sit amet facilisis id, vehicula sed turpis. Sed faucibus odio a convallis porttitor. Curabitur ut odio consectetur ligula vulputate facilisis. Praesent volutpat pellentesque purus at lobortis. Suspendisse quis enim a libero condimentum consequat. Donec consequat magna nec tellus venenatis sollicitudin. Proin dui justo, mollis sed sodales eget, laoreet sed nisl. Curabitur vehicula est ac eros condimentum pellentesque.",
+                                "imgpath":"../../../assets/team/megan.jpg"
                         },
                         {
                                 "name":"Blake",
-                                "bio":"none",
-                                "imgpath":"../../../assets/team/blake.jpg"
+                                "bio":"he also a G",
+                                "imgpath":'../../../assets/team/blake.jpg'
+                        },
+                        {
+                                "name":"Josh",
+                                "bio":"he also a G",
+                                "imgpath":'../../../assets/team/josh.jpg'
+                        },
+                        {
+                                "name":"Josh",
+                                "bio":"he also a G",
+                                "imgpath":'../../../assets/team/josh.jpg'
+                        },
+                        {
+                                "name":"Josh",
+                                "bio":"he also a G",
+                                "imgpath":'../../../assets/team/josh.jpg'
+                        },
+                        {
+                                "name":"placeholder",
+                                "bio":"don't remove",
+                                "imgpath":'just dont'
                         }
                 ]
         };
@@ -24,15 +49,28 @@ export class AboutComponent implements OnInit {
         constructor() { }
 
         ngOnInit() {
-                for (var i = 0; i < this.imgConfig.people.length; i++) {
-                        this.generateImageLine(i);
+                let carry = 0;
+                      
+                for (var person = 0; person < this.imgConfig.people.length; person++) {
+                        var rowContainer = document.createElement("DIV");
+                        var style = document.createAttribute("style");
+                        style.value = "display: flex; justify-content: center; align-items: center; flex-direction: row";
+                        rowContainer.setAttributeNode(style);
+                        
+                        for (var i = 0; i <= 2; i++) {
+                                rowContainer.appendChild(this.generateImageLine(person + i));
+                                carry = (person + i);
+                        }
+                        person = carry;
+                        document.getElementById("master").appendChild(rowContainer);
                 }
         }
         
         generateImageLine(i) {
+                
                 var container = document.createElement("DIV");  
                 var margin = document.createAttribute("style");
-                margin.value = "margin-right: 25px; margin-bottom: 25px; display: flex; justify-content: center; align-items: center; flex-direction: column; color:white; font-family: 'Open Sans', sans-serif";
+                margin.value = "margin-right: 25px; margin-bottom: 25px; display: flex; justify-content: center; align-items: center; flex-direction: column; color:white; font-family: 'Open Sans', sans-serif; width: 350px;";
                 container.setAttributeNode(margin);
                 
                 // create an image
@@ -60,6 +98,6 @@ export class AboutComponent implements OnInit {
                 container.appendChild(profile);
                 container.appendChild(name);
                 
-                document.getElementById("master").appendChild(container);
+                return container;
         }
 }
