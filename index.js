d3f9d1e6fbbb123c7d91ca25809395f6f7a65bd8
stlgolfer <stlgolfer@users.noreply.github.com>
2017-10-05 10:48:32 -0500

    Add Bad Signin Endpoint

--- index.js
+++ index.js
@@ -1,10 +1,29 @@
 var express = require('express');
 var app = express();
 
+var bodyParser = require('body-parser');
+//var jsonParser = bodyParser.json();
+
+var database = require(__dirname + '/timelogger/logs.json');
+
 app.set('view engine', 'ejs');
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended : false }));
 
 app.get('/', function (req, res) {
 	res.render('pages/index');
+})
+
+var date = new Date();
+database.meetings.push(date.toString());
+
+app.post('/time/signin', function (req, res) {
+	var template = {
+		"member":"",
+		"sigin":"",
+		"signout":""
+	}
+	res.end(member);
 })
 
 app.get('/about', function (req, res) {
