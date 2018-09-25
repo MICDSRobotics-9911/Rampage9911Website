85f84e38d30a7e85040caab6a5c7a681fdeba096
stlgolfer <amigala@micds.org>
2018-09-25 17:12:20 -0500

    Changes from last time

--- routing.js
+++ routing.js
@@ -1,24 +1,47 @@
 module.exports = (app) => {
+	/**
+	 * Main page endpoint
+	 */
 	app.get('/', (req, res) => {
 		res.render('pages/index');
 	});
 
+	/**
+	 * About page endpoint
+	 * @name about
+	 */
 	app.get('/about', (req, res) => {
 		res.render('pages/about');
 	});
 
+	/**
+	 * Contact page endpoint
+	 * @name contact
+	 */
 	app.get('/contact', (req, res) => {
 		res.render('pages/contact');
 	});
 
+	/**
+	 * Programming page endpoint
+	 * @name programming
+	 */
 	app.get('/programming', (req, res) => {
 		res.render('pages/programming');
 	});
 
+	/**
+	 * Robot page endpoint
+	 * @name robot
+	 */
 	app.get('/robot', (req, res) => {
 		res.render('pages/robot');
 	});
-	
+
+	/**
+	 * FIRST page endpoint
+	 * @name first
+	 */
 	app.get('/first', (req, res) => {
 		res.render('pages/first');
 	});
