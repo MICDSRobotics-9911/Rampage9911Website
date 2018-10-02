4d57fcf91fdc30efa8b3821210d098029eb669fc
stlgolfer <amigala@micds.org>
2018-10-02 18:48:01 -0500

    Update travis

--- deploy.sh
+++ deploy.sh
@@ -3,5 +3,5 @@ chmod 600 deploy_rsa # Allow read access to the private key
 ssh-add deploy_rsa # Add the private key to SSH
 
 git config --global push.default matching
-git remote add deploy ssh://git@$IP:$PORT$DEPLOY_DIR
+git remote add deploy ssh://root@$IP:$PORT$DEPLOY_DIR
 git push deploy master
