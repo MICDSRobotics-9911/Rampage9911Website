721b9d694cd0e30cfbc257804d8f6c40695d4159
stlgolfer <amigala@micds.org>
2018-10-02 18:53:16 -0500

    Deployment Just Doesn't work

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,1 @@
-eval "$(ssh-agent -s)" # Start ssh-agent cache
-chmod 600 deploy_rsa # Allow read access to the private key
-ssh-add deploy_rsa # Add the private key to SSH
-
-git config --global push.default matching
-git remote add deploy ssh://root@$IP:$PORT$DEPLOY_DIR
-git push deploy master
+echo deploy
