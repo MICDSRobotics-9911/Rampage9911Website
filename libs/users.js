20e46e8a361496ea4ed21b2873035dea350d6ff5
Nick Clifford <nick@nickclifford.me>
2018-10-18 17:08:16 -0500

    fix SQL injection attack

--- libs/users.js
+++ libs/users.js
@@ -1,5 +1,5 @@
 module.exports = (app, db) => {
-        /*app.get('/account/create', (req, res) => {
+	/*app.get('/account/create', (req, res) => {
                 db.run(`INSERT INTO userdata (username, password, email) VALUES ('${req.param('name')}', '${req.param('password')}', '${req.param('email')}')`, [], function (error) {
                         if (error) {
                                 console.log(error);
@@ -17,21 +17,21 @@ module.exports = (app, db) => {
                 })
         })*/
 
-        app.post('/email/submit', (req, res) => {
-                db.run(`INSERT INTO maillist (email) VALUES ('${req.param('email')}')`, [], function (error) {
-                        if (error) {
-                                console.log(error);
-                                res.json({
-                                        error: true,
-                                        message: error
-                                })
-                        }
-                        else {
-                                res.json({
-                                        error: false,
-                                        message: null
-                                })
-                        }
-                })
-        })
-}
+	app.post('/email/submit', (req, res) => {
+		db.run('INSERT INTO maillist (email) VALUES (?)', req.body.email, (error) => {
+			if (error) {
+				console.log(error);
+				res.json({
+					error: true,
+					message: error
+				});
+			}
+			else {
+				res.json({
+					error: false,
+					message: null
+				});
+			}
+		});
+	});
+};
